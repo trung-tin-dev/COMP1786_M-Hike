a253342532b095255a0ebac73f5b7179290956ea
Tạ Đức Trung Tín <tatin3469@gmail.com>
2026-07-09 21:16:35 +0700

    nav-bottom

--- app/src/main/java/com/example/m_hike/MainActivity.java
+++ app/src/main/java/com/example/m_hike/MainActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.example.m_hike.fragments.HomeFragment;
+import com.example.m_hike.fragments.HikeFragment;
+import com.example.m_hike.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigation =
+                findViewById(R.id.bottomNavigation);
+        replaceFragment(new HomeFragment());
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.nav_home) {
+
+                replaceFragment(new HomeFragment());
+
+            } else if (item.getItemId() == R.id.nav_hike) {
+
+                replaceFragment(new HikeFragment());
+
+            } else if (item.getItemId() == R.id.nav_profile) {
+
+                replaceFragment(new ProfileFragment());
+
+            }
+
+            return true;
+
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+
+    }
+
+
 }
